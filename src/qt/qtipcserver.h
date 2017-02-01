184e82c88a8f3ee9405804f5ab965bf0a9689550
mail <you@example.com>
2017-02-01 10:13:48 +0100

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define hollyweed-qt message queue name
+#define BITCOINURI_QUEUE_NAME "hollyweedURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
